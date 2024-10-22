2b26bf834150165ab9311f3b49c2c91c21171e9b
Abhiram <write4abhiram@gmail.com>
2024-10-22 09:09:56 -0700

    Update padding and gap

--- src/Components/Presenters/Presenters.tsx
+++ src/Components/Presenters/Presenters.tsx
@@ -24,7 +24,7 @@ const Presenters: React.FC = () => {
       </header>
 
       {/* Presenters Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 lg:gap-6 lg:p-6">
         {presentersData.map((presenter, index) => (
           <div
             key={index}
