c43da75d06d42b9eb835915c9ab21de1b414a8fa
Yogesh Manni <yogeshmanni786@gmail.com>
2024-11-19 17:57:37 -0800

    fix : navigate to homepage on clicking logo

--- src/Components/LogoComponent.tsx
+++ src/Components/LogoComponent.tsx
@@ -2,15 +2,18 @@
 
 import React from "react";
 import logo from "../assets/img/logo.png";
+import { Link } from "react-router-dom";
 
 const LogoComponent: React.FC = () => {
   return (
     <div className="h-[auto] auto m-5 flex items-center justify-center">
-      <img
-        src={logo}
-        className="h-[100px] rounded-full w-auto"
-        alt="logo"
-      ></img>
+      <Link to="/">
+        <img
+          src={logo}
+          className="h-[100px] rounded-full w-auto"
+          alt="logo"
+        ></img>
+      </Link>
     </div>
   );
 };
