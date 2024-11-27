96b7822251d214180a1c3b74a20f5d8185f4b8fe
Abhiram <write4abhiram@gmail.com>
2024-11-26 21:39:45 -0800

    Refactor hero

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -3,16 +3,7 @@ import homeData from "../../data/homeData";
 
 import Conference from "./Conference";
 import Programs from "./Programs";
-
-interface HeadingProps {
-  tagline: string;
-  subHeading: string;
-  src: string;
-  callToAction: {
-    text: string;
-    link: string;
-  };
-}
+import Hero from "./Hero";
 
 interface SectionProps {
   tagline: string;
@@ -24,47 +15,6 @@ interface SectionProps {
     link: string;
   };
 }
-
-const Heading: React.FC<HeadingProps> = ({
-  tagline,
-  subHeading,
-  src,
-  callToAction,
-}) => {
-  return (
-    <div className="relative w-full h-screen">
-      <img
-        className="w-full h-full object-cover"
-        src={src}
-        alt="Comox Valley Programs"
-      />
-      <div className="absolute inset-0 flex items-center p-8 lg:pl-24">
-        <div className="bg-black bg-opacity-75 max-w-2xl rounded-lg p-6">
-          <h1 className="text-white font-bold text-5xl md:text-6xl">
-            {tagline}
-          </h1>
-          <p className="text-white text-lg md:text-xl mt-4">{subHeading}</p>
-          <a
-            href={callToAction.link}
-            className="custom-button Btn mt-8 inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition"
-          >
-            {callToAction.text}
-          </a>
-        </div>
-      </div>
-      <a href="#main">
-        <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 flex flex-col items-center">
-          <div className="w-12 h-12 flex items-center justify-center rounded-full bg-headingColor bg-opacity-80">
-            <span className="animate-bounce text-white text-4xl font-bold">
-              ↓
-            </span>
-          </div>
-          <p className="font-bold text-white text-sm mt-2">Scroll down</p>
-        </div>
-      </a>
-    </div>
-  );
-};
 
 // Section component with flexible layout
 const Section: React.FC<SectionProps> = ({
@@ -107,7 +57,7 @@ const Home: React.FC = () => {
   return (
     <>
       <div className="relative">
-        <Heading
+        <Hero
           tagline={homeData[0].headline}
           subHeading={homeData[0].subheadline}
           src={homeData[0].backgroundImage}
